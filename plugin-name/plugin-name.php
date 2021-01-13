5e607b37356c4c0c3e361966ac090403be88f0b6
John O'Flaherty <moonthatspells@gmail.com>
2021-01-13 21:52:35 +0000

    Starting to Personalize

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -8,20 +8,20 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              http://jayoheff.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           JayOhEff
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin Name:       Boilerplate WordPress Plugin 
+ * Plugin URI:        
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author URI:        
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       jayoheff
  * Domain Path:       /languages
  */
 
